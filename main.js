9a16239fe56c8a395f02f8bbfc8241e57e87f36e
Matthew Yang <yang70@gmail.com>
2019-04-04 12:51:10 -0700

    done

--- main.js
+++ main.js
@@ -1,15 +1,49 @@
-// This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
-function AppViewModel() {
-  this.firstName = ko.observable( "Bert" );
-  this.lastName  = ko.observable( "Bertington" );
-  this.fullName  = ko.computed( function() {
-    return this.firstName() + " " + this.lastName();
-  }, this);
-  this.capitalizeLastName = function() {
-    let currentVal = this.lastName();
-    this.lastName( currentVal.toUpperCase() );
-  }
+// Class to represent a row in the seat reservations grid
+function SeatReservation( name, initialMeal ) {
+  var self  = this;
+  self.name = name;
+  self.meal = ko.observable( initialMeal );
+  
+  self.formattedPrice = ko.computed( function() {
+    var price = self.meal().price;
+    return price ? "$" + price.toFixed( 2 ) : "None";
+  });
 }
 
-// Activates knockout.js
-ko.applyBindings( new AppViewModel() );
+// Overall viewmodel for this screen, along with initial state
+function ReservationsViewModel() {
+  var self = this;
+  
+  // Non-editable catalog data - would come from the server
+  self.availableMeals = [
+    { mealName: "Standard (sandwich)", price: 0 },
+    { mealName: "Premium (lobster)", price: 34.95 },
+    { mealName: "Ultimate (whole zebra)", price: 290 }
+  ];    
+  
+  // Editable data
+  self.seats = ko.observableArray([
+    new SeatReservation( "Steve", self.availableMeals[ 0 ] ),
+    new SeatReservation( "Bert", self.availableMeals[ 0 ] )
+  ]);
+  
+  self.addSeat = function() {
+    self.seats.push( new SeatReservation( "", self.availableMeals[ 0 ] ) );
+  }
+  
+  self.removeSeat = function( seat ) {
+    self.seats.remove( seat );
+  }
+  
+  self.totalSurcharge = ko.computed( function() {
+    var total = 0;
+    
+    for ( var i = 0; i < self.seats().length; i++ ) {
+      total += self.seats()[ i ].meal().price;
+    };
+    
+    return total;
+  });
+}
+
+ko.applyBindings( new ReservationsViewModel() );
